fc991d5595b878cea805d28a73635e9bfb52df8d
jafffe <49226786+jafffe@users.noreply.github.com>
2019-04-27 15:21:33 +0800

    kcfdemo

---
+++ KCFcameral/runtracker.cpp
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <tchar.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "kcftracker.hpp"
+
+using namespace std;
+using namespace cv;
+bool drawing_box = false;
+bool gotBB = false;
+Rect box;
+//画矩形框
+void drawBox(Mat& image, CvRect box, Scalar color, int thick) {
+	rectangle(image, cvPoint(box.x, box.y), cvPoint(box.x + box.width, box.y + box.height), color, thick);
+}
+//鼠标追踪函数
+void mouseHandler(int event, int x, int y, int flags, void *param) {
+	switch (event) {
+	case CV_EVENT_MOUSEMOVE:
+		if (drawing_box) {
+			box.width = x - box.x;
+			box.height = y - box.y;
+		}
+		break;
+	case CV_EVENT_LBUTTONDOWN:
+		drawing_box = true;
+		box = Rect(x, y, 0, 0);
+		break;
+	case CV_EVENT_LBUTTONUP:
+		drawing_box = false;
+		if (box.width < 0) {
+			box.x += box.width;
+			box.width *= -1;
+		}
+		if (box.height < 0) {
+			box.y += box.height;
+			box.height *= -1;
+		}
+		gotBB = true;   //已经获得bounding box  
+		break;
+	}
+}
+int main(int argc, char* argv[]) {
+	VideoCapture capture;//打开摄像头函数
+	capture.open(0);
+	if (!capture.isOpened())
+	{
+		cout << "capture device failed to open!" << endl;
+		return 1;
+	}
+	//Register mouse callback to draw the bounding box  
+	cvNamedWindow("KCF", CV_WINDOW_AUTOSIZE);
+	cout << "11111" << endl;
+	cvSetMouseCallback("KCF", mouseHandler, NULL);  //用鼠标选中初始目标的bounding box  
+	cout << "22222" << endl;
+
+	bool HOG = true;//是否使用HOG颜色直方图特征
+	bool FIXEDWINDOW = false;//是否使用固定窗口大小
+	bool MULTISCALE = true;//是否使用多尺度
+	bool LAB = false;//是否使用lab色颜色空间，类比rgb颜色
+	bool SILENT = false;
+	// Create KCFTracker object
+	KCFTracker tracker(HOG, FIXEDWINDOW, MULTISCALE, LAB);
+	// Frame readed
+	Mat frame;
+	// Tracker results
+	Rect result;
+
+	///Initialization  
+	while (!gotBB)
+	{
+		cout << "aaaaa" << endl;
+		capture >> frame;
+		drawBox(frame, box, (0, 0, 255), 2);  //把bounding box 画出来  
+		imshow("KCF", frame);
+		if (cvWaitKey(33) == 'q')
+			return 0;
+	}
+	//Remove callback  
+	cvSetMouseCallback("KCF", NULL, NULL);  //如果已经获得第一帧用户框定的box了，就取消鼠标响应  
+	cout << "33333" << endl;
+	printf("Initial Bounding Box = x:%d y:%d h:%d w:%d\n", box.x, box.y, box.width, box.height);
+	tracker.init(box, frame);//初始化追踪框/////////////////////////////////// tracker.init 初始化目标的在循环体外面
+	////循环开始循环开始////
+	while (capture.read(frame))// 一直把capture里的图像read进 frame里面去，开始跟踪目标
+	{
+		result = tracker.update(frame);//追踪///////////////////////////////////// tracker.update 
+		//画矩形框
+		rectangle(frame, Point(result.x, result.y), Point(result.x + result.width, result.y + result.height), Scalar(0, 255, 255), 1, 8);////////////////////////////////////////////// 把result的结果画在当前图像上
+		if (!SILENT) {
+			imshow("KCF", frame);
+			waitKey(10);
+		}
+	}
+	////循环开始循环结束////
+}
